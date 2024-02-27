b97b22e1a32c51c685d0ba6aac1a1720876f1672
RaviCarrillo <ravicarrillo@gmail.com>
2024-02-27 20:54:11 -0300

    alteração no buyWeapon

--- script.js
+++ script.js
@@ -117,6 +117,17 @@ function buyWeapon() {
     }
 }
 
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    }
+}
+
+
 function fightDragon() {
 
 }
