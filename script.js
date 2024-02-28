f25ceb5a14fb34d751db2582968cd7b1289c375c
RaviCarrillo <ravicarrillo@gmail.com>
2024-02-28 16:46:13 -0300

    update funtion attack

--- script.js
+++ script.js
@@ -108,6 +108,10 @@ function goCave() {
 function goFight() {
     update(locations[3]);
     monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+    
 }
 
 function buyHealth() {
@@ -176,7 +180,15 @@ function fightDragon() {
 }
 
 
-function attack() {}
+function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    monsterHealth -= weapons[currentWeapon].power;
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+}
 
 function dodge() {}
 
